886412d668232c14f61faa47bcddc97797e4e938
Rich Ryan <richryan@csub.edu>
2026-05-29 19:30:27 -0700

    change push_button_replication.R

--- push_button_replication.R
+++ push_button_replication.R
@@ -3,18 +3,9 @@ if (!file.exists("_targets.R")) {
   stop("Run this script from the project root, where _targets.R is located.", call. = FALSE)
 }
 
-if (file.exists("renv.lock")) {
-  if (!requireNamespace("renv", quietly = TRUE)) {
-    stop(
-      "This project uses renv, but the renv package is not installed.\n",
-      "Please install it first by running:\n\n",
-      "install.packages('renv')\n\n",
-      "Then rerun this script.",
-      call. = FALSE
-    )
-  }
-  
-  renv::restore(prompt = FALSE)
-}
+# Make sure same R environment
+renv::restore()
+# I had to run the following code in Posit Cloud directory
+# update.packages(ask = FALSE, checkBuilt = TRUE)
 
 targets::tar_make()
